293eafa0e17bf21d48e6b24b0034942457b64054
SaiPallavi7989 <161013366+SaiPallavi7989@users.noreply.github.com>
2024-08-08 13:18:39 +0530

    Update Music Store Data Analysis.sql

--- music store data/Music Store Data Analysis.sql
+++ music store data/Music Store Data Analysis.sql
@@ -99,26 +99,17 @@ A CTE is a temporary result set that you can reference within a SELECT, INSERT, 
 ROW_NUMBER() OVER(PARTITION BY customer.country ORDER BY COUNT(invoice_line.quantity) DESC) AS RowNo: 
 Assigns a row number to each genre within each country, ordered by the number of purchases in descending order.*/
 
-/*Country along with customers and how much they spent*/
-with recursive 
-customer_with_country as(
-select customer.customer_id,first_name,last_name,billing_country,sum(total) as total_spending
+/*11.Country along with customers and how much they spent*/
+with customer_with_country as(
+select customer.customer_id,first_name,last_name,billing_country,sum(total) as total_spending,
+row_number() over(partition by billing_country order by sum(total) desc) as RowNo
 from invoice
 join customer on customer.customer_id=invoice.customer_id
 group by 1,2,3,4
-order by 2,3 desc),
+order by 2,3 desc)
 
-country_max_spending as(
-select billing_country,max(total_spending) as max_spending
-from customer_with_country
-group by billing_country)
+select * from customer_with_country where RowNo<=1;
 
-select cc.billing_country,cc.total_spending,cc.first_name,cc.last_name,cc.customer_id
-from customer_with_country cc
-join country_max_spending ms
-on cc.billing_country=ms.billing_country
-where cc.total_spending= ms.max_spending
-order by  1;
 
 
 
